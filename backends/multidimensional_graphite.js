199969040f196183cb6090d7a02befce894f9a1f
Ryan Neal <ryanneal@yelp.com>
2015-04-14 18:01:28 -0700

    basics for test

---
+++ backends/multidimensional_graphite.js
@@ -0,0 +1,110 @@
+var log = console; // the logger
+var debug;
+
+var events = require('events');
+
+
+function MultiDimensionalGraphite(startupTime, config, serverEmitter) {
+  log.info('Starting up Multidimensional Graphite instance at ' + startupTime);
+
+  this.config = config;
+  this.innerEmitter = new events.EventEmitter();
+  this.actualGraphite = require('./graphite');
+  this.actualGraphite.init(startupTime, config, this.innerEmitter, log);
+  var self = this;
+
+  serverEmitter.on('flush', function(ts, metrics) {
+    var transdmetrics = self.transformMetrics(metrics);
+    self.innerEmitter.emit('flush', transdmetrics);
+  });
+};
+
+/*
+ * This is what actually transforms the name. It converts the string to JSON,
+ * then pulls the 'graphite_keys' and for each part it gets the value and
+ * then builds the final graphite key as: ``key=value.key2=value2``
+ *
+ * Some errors resulting in the original name being returned
+ *  - not JSON
+ *  - doesn't have the 'graphite_keys' key
+ *
+ * Errors which are *really* errors:
+ *  - if the key from 'graphite_keys' DNE
+ */
+MultiDimensionalGraphite.prototype.transformKey = function(name) {
+  var parsed = undefined
+  try {
+    parsed = JSON.parse(name);
+  } catch(err) {
+    return name;
+  }
+  var mapVersion = {};
+  parsed.forEach(function(item) {
+    mapVersion[item[0]] = item[1];
+  });
+
+  var gKeys = mapVersion['graphite_keys'];
+  if(gKeys === undefined) {
+    return name;
+  }
+
+  finalKeys = [];
+
+  gKeys.split(';').forEach(function(part) {
+    var val = mapVersion[part];
+    if(val === undefined) {
+      log.error('Poorly formed Key detected, original key: "' + name +
+        '", \ngraphite_keys: "' + gKeys +
+        '", \nmissing value at key: "' + part + '"');
+      return;
+    } else {
+      finalKeys.push([part, val].join('='));
+    }
+  });
+
+  if(finalKeys.length == 0) {
+    log.error("Failed to transform the name '" + name + "'");
+    return undefined;
+  }
+
+  return finalKeys.join('.');
+};
+
+MultiDimensionalGraphite.prototype.transformMetrics = function(metrics) {
+  var transMetrics = {};
+  var transKey = this.transformKey;
+  var transObj = function(obj) {
+    var finalObj = {};
+    for(var k in obj) {
+      if(obj.hasOwnProperty(k)) {
+        var newKey = transKey(k);
+        finalObj[newKey] = obj[k];
+      }
+    }
+    return finalObj;
+  };
+
+  transMetrics['counters'] = transObj(metrics['counters']);
+  transMetrics['gauges'] = transObj(metrics['gauges']);
+  transMetrics['sets'] = transObj(metrics['sets']);
+  transMetrics['timers'] = transObj(metrics['timers']);
+  transMetrics['timer_data'] = transObj(metrics['timer_data']);
+  transMetrics['counter_rates'] = transObj(metrics['counter_rates']);
+  transMetrics['pctThreshold'] = metrics['pctThreshold'];
+
+  return transMetrics;
+};
+
+exports.multid_prototype = MultiDimensionalGraphite.prototype;
+
+exports.init = function graphite_init(startupTime, config, events, logger) {
+  debug = config.debug;
+  log = logger;
+  log.__proto__.debug = function(msg) {
+    if(debug) {
+      l.log(msg);
+    }
+  };
+
+  return new MultiDimensionalGraphite(startupTime, config, events);
+};
---
+++ test/multi_graphite_tests.js
@@ -0,0 +1,184 @@
+var multid = require('../backends/multidimensional_graphite.js');
+
+function createConfig() {
+  return {
+    debug: true,
+    flush_counts: true,
+  };
+}
+
+// squash the output
+function getLogger() {
+  return {
+    log: function(msg, level) { ; },
+    info: function(msg, level) { ; },
+    error: function(msg, level) { ; },
+  }
+  //return console;
+}
+
+// creates obj that we can use to sim the actual sending of metrics
+function createEmitter() {
+  var emitter = {
+    on: function(key, listener) {
+      // when a method is reg'd add it in so we can call it directly
+      this.__proto__[key] = listener;
+    }
+  };
+  return emitter;
+}
+
+function compareObjs(test, actual, expected) {
+  test.ok(actual !== undefined, "Actual is undefined");
+  test.ok(expected !== undefined, "expected is undefined");
+
+  var actualKeys = 0;
+  for(aKey in actual) {
+    if(actual.hasOwnProperty(aKey)) {
+      actualKeys += 1;
+      test.ok(expected[aKey] !== undefined, "Expected doesn't expect key " + aKey);
+      test.deepEqual(actual[aKey], expected[aKey], "difference at key " + aKey);
+    }
+  }
+
+  var expectedKeys = 0;
+  for(eKey in expected){
+    if(expected.hasOwnProperty(eKey)) {
+      expectedKeys += 1;
+    }
+  }
+
+  test.equal(actualKeys, expectedKeys, "Mismatch in the number of keys");
+};
+
+// ---------------------------------------------------------------------------
+// TESTS
+// ---------------------------------------------------------------------------
+module.exports.testCreation = function(test) {
+  var inst = multid.init(0, createConfig(), createEmitter(), getLogger());
+  test.ok(inst !== undefined, 'Failed to create a Multidimensional Graphite instance');
+  test.done();
+};
+
+module.exports.testKeyTransformation = function(test) {
+  proto = multid.multid_prototype;
+
+  var testVal = 'this.isnt.json';
+  var actual = proto.transformKey(testVal);
+  test.equal(actual, 'this.isnt.json');
+
+  // doesn't have the right parts
+  testVal = '[["graphite_keys", "host;port"]]';
+  actual = proto.transformKey(testVal);
+  test.equal(actual, undefined);
+
+  // all parts present
+  testVal =
+    '[ \
+    ["graphite_keys", "host;port"], \
+    ["host", "somehost"], \
+    ["port", "someport"] \
+    ]';
+  actual = proto.transformKey(testVal);
+  test.equal(actual, 'host=somehost.port=someport');
+
+  test.done();
+};
+
+module.exports.testTransformation = function(test) {
+  var inMetrics = {
+    counters: {
+      'statsd.bad_lines_seen': 1,
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "myhost"], \
+        ["package", "somepackage"], \
+        ["port", "23"]\
+      ]': 200
+    },
+    timers: {
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "timerhost"], \
+        ["package", "timerpackage"], \
+        ["port", "342"]\
+      ]': [2, 3, 4]
+    },
+    gauges: {
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "gaugehost"], \
+        ["package", "gaugepackage"], \
+        ["port", "546"]\
+      ]': 222
+    },
+    sets: {
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "sethost"], \
+        ["package", "setpackage"], \
+        ["port", "546"]\
+      ]': 222
+    },
+    timer_data: {
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "timerhost"], \
+        ["package", "timerpackage"], \
+        ["port", "342"]\
+      ]': {
+        count_90: 1,
+        mean_90: 2,
+        upper_90: 3,
+        sum_90: 4
+      }
+    },
+    counter_rates: {
+      'statsd.bad_lines_seen': 1,
+      '[["graphite_keys", "host;port;package"], \
+        ["host", "myhost"], \
+        ["package", "somepackage"], \
+        ["port", "23"]\
+      ]': 0.1
+    },
+    pctThreshold: [90]
+  };
+
+  var expected= {
+    counters: {
+      'statsd.bad_lines_seen': 1,
+      'host=myhost.port=23.package=somepackage': 200
+    },
+    timers: {
+      'host=timerhost.port=342.package=timerpackage': [2, 3, 4]
+    },
+    gauges: {
+      'host=gaugehost.port=546.package=gaugepackage': 222
+    },
+    sets: {
+      'host=sethost.port=546.package=setpackage': 222
+    },
+    timer_data: {
+      'host=timerhost.port=342.package=timerpackage': {
+        count_90: 1,
+        mean_90: 2,
+        upper_90: 3,
+        sum_90: 4
+      }
+    },
+    counter_rates: {
+      'statsd.bad_lines_seen': 1,
+      'host=myhost.port=23.package=somepackage': 0.1
+    },
+    pctThreshold: [90]
+  };
+
+  proto = multid.multid_prototype;
+  var outMetrics = proto.transformMetrics(inMetrics);
+  compareObjs(test, outMetrics['counters'], expected['counters']);
+  compareObjs(test, outMetrics['gauges'], expected['gauges']);
+  compareObjs(test, outMetrics['sets'], expected['sets']);
+  compareObjs(test, outMetrics['counter_rates'], expected['counter_rates']);
+  compareObjs(test, outMetrics['pctThreshold'], expected['pctThreshold']);
+  compareObjs(test, outMetrics['timers'], expected['timers']);
+  compareObjs(test, outMetrics['timer_data'], expected['timer_data']);
+
+  test.done();
+}
+
+module.exports.testMissingKeys = function(test) { test.done(); }
